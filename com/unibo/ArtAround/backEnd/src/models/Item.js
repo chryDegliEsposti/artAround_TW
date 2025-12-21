8558d055bbc761f35c53be0e8e1de91622ad311a
AbaSkillzz <ababilhossain5@gmail.com>
2025-12-21 18:02:28 +0100

    1)sistemato mongoose scheme per obj Visita, 2)tolto schema Author pk e' un Item

--- com/unibo/ArtAround/backEnd/src/models/Item.js
+++ com/unibo/ArtAround/backEnd/src/models/Item.js
@@ -1,20 +1,65 @@
 const mongoose = require('mongoose');
 
 const ItemSchema = new mongoose.Schema({
+    itemType: {
+        type: String,
+        enum: ['artwork', 'artist', 'style', 'movement', 'historical_event', 'other'],
+        required: true,
+        default: 'artwork'
+    },
+
+    artworkId: {
+        type: String,
+        required: () => {this.itemType === 'artwork'},
+        index: true //per ottimizzazioni db
+        // Esempio: "Q126599960" (per il quadro di Bedoli)
+    },
+
     title: { type: String, required: true },
-    description_short_easy: { type: String },
-    description_short_medium: { type: String },
-    description_short_hard: { type: String },
-    description_medium_easy: { type: String },
-    description_medium_medium: { type: String },
-    description_medium_hard: { type: String },
-    description_long_easy: { type: String },
-    description_long_medium: { type: String },
-    description_long_hard: { type: String },
 
-    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
-    museums: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Museum' }],
+    recognitionImage: {
+        type: String, //url dell'immagine...
+    },
 
+    length: {
+        type: String,
+        enum: ['3s', '15s', '1min', '4min'], 
+        required: true
+    },
+    
+    languageLevel: {
+        type: String,
+        enum: ['infantile', 'elementare', 'medio', 'specialistico'],
+        required: true
+    },
+
+    description: { type: String, required: true },
+
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true },
+
+    syle: { type: mongoose.Schema.Types.ObjectId, ref: 'Item' },
+
+    museum: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Museum', 
+        required : true 
+    },
+
+    license: { 
+        type: String, 
+        enum: [
+            'CC0',           // Public domain
+            'CC BY',         // Uso libero con attribuzione
+            'CC BY-SA',      // Uso libero con stessa licenza
+            'CC BY-NC',      // Non commerciale
+            'CC BY-ND',      // No modifiche
+            'CC BY-NC-SA',   // Non commerciale + stessa licenza
+            'CC BY-NC-ND',   // Non commerciale + no modifiche
+            'Proprietary',   // Tutti i diritti riservati
+            'Custom'         // Licenza personalizzata
+        ],
+        required: () => {this.itemType === 'artwork'},
+    },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Item', ItemSchema);
--- com/unibo/ArtAround/backEnd/src/models/Museum.js
+++ com/unibo/ArtAround/backEnd/src/models/Museum.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MuseumSchema = new mongoose.Schema({
-    title: { type: String, required: true },
+    name: { type: String, required: true },
     description: { type: String },
     address: { type: String },
     webSite: { type: String },
