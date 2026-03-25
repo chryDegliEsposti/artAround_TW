f5221f049bc52dba721d2a547054333e583f242f
Mattelainz <matteomartin55@gmail.com>
2026-03-25 16:37:55 +0100

    Delete com/unibo/ArtAround directory

--- com/unibo/ArtAround/backEnd/src/models/VisitDone.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const VisitDoneSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    visit: { type: mongoose.Schema.Types.ObjectId, ref: 'Visit', required: true },
-    day: { type: Date, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('VisitDone', VisitDoneSchema);
